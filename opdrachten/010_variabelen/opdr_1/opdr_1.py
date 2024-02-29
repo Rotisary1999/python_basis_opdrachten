8229f0e083b5e7b6ccd16344eadf9ae767198365
Rotisary1999 <97038433@st.deltion.nl>
2024-02-29 15:18:46 +0100

    Opdracht 2 en 3

--- opdrachten/010_variabelen/opdr_1/opdr_1.py
+++ opdrachten/010_variabelen/opdr_1/opdr_1.py
@@ -1,5 +1,5 @@
 # Opdracht 1
-# Naam student:
+# Naam student: Roisin O'Hagan
 # Groep:
 
 # Hier komt je code...
--- opdrachten/010_variabelen/opdr_2/opdr_2.py
+++ opdrachten/010_variabelen/opdr_2/opdr_2.py
@@ -1,7 +1,8 @@
 # Opdracht 1
-# Naam student:
+# Naam student: Roisin O'Hagan
 # Groep:
 
 # Hier komt je code...
 
-my_tuple = ...
+my_tuple = (11, "Oh een tuple", 13.5, 8009)
+print(my_tuple)
