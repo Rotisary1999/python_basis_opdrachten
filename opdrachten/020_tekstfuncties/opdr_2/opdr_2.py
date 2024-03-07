639e4b12130209be24c64493db4da3610e76746f
Rotisary1999 <97038433@st.deltion.nl>
2024-03-07 14:34:55 +0100

    Opdracht 2 Tekstfuncties

--- opdrachten/020_tekstfuncties/opdr_2/opdr_2.py
+++ opdrachten/020_tekstfuncties/opdr_2/opdr_2.py
@@ -1,9 +1,10 @@
 # Opdracht 1
-# Naam student:
+# Naam student: Roisin O'Hagan
 # Groep:
 
 # Hier komt je code...
 
 my_list = ...
 
-print()
+my_string = "Tinus gaat op zijn tandem naar de hottentottententoonstelling"
+print (f"De letter 't' komt {my_string.count('t')}x voor in my_string") 
