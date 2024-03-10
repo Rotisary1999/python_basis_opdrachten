4cc40193d8a580a86f192dd0b168f72369432f53
Rotisary1999 <97038433@st.deltion.nl>
2024-03-10 17:06:52 +0100

    Opdracht 1 lists

--- opdrachten/040_lists/opdr_1/opdr_1.py
+++ opdrachten/040_lists/opdr_1/opdr_1.py
@@ -1,9 +1,17 @@
 # Opdracht 1 lists
-# Naam student:
+# Naam student: Roisin
 # Groep:
 
-mylist = ...
-dict_1 = ...
-dict_2 = ...
-dict_3 = ...
-dict_4 = ...
+my_dict1 = {"id" : "0", "voornaam" : "Niels" , "achternaam" : "Riezebos"  },
+my_dict2 = {"id" : "1", "voornaam" : "Denise", "achternaam" : "van Keeken"},
+my_dict3 = {"id" : "2", "voornaam" : "Ian" , "achternaam" : "Ohagan"},
+my_dict4 = {"id" : "3", "voornaam" : "Leslie" , "achternaam" : "Heirbaut"},
+
+my_list = [
+    {"id" : "0", "voornaam" : "Niels" , "achternaam" : "Riezebos"  },
+    {"id" : "1", "voornaam" : "Denise", "achternaam" : "van Keeken"  },
+    {"id" : "3", "voornaam" : "Ian" , "achternaam" : "Ohagan"  },
+    {"id" : "4", "voornaam" : "Leslie" , "achternaam" : "Heirbaut"  },
+]
+
+print(my_list[1]["voornaam"], my_list[1]["achternaam"])
