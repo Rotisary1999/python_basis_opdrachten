011f75b4a6c67cfae9dc3157a10216fad7ec40a7
Rotisary1999 <97038433@st.deltion.nl>
2024-03-10 16:17:05 +0100

    Opdracht 2 berekeningen

--- opdrachten/030_berekeningen/opdr_1/opdr_1.py
+++ opdrachten/030_berekeningen/opdr_1/opdr_1.py
@@ -1,5 +1,12 @@
 # Opdracht 1 berekeningen
-# Naam student:
+# Naam student: Roisin 
 # Groep:
 
 # Hier komt je code...
+
+joe = 23
+bob = 15
+fred = 22
+
+print (joe + bob + fred)
+print ((joe + bob + fred)/3)
--- opdrachten/030_berekeningen/opdr_2/opdr_2.py
+++ opdrachten/030_berekeningen/opdr_2/opdr_2.py
@@ -1,10 +1,23 @@
 # Opdracht 2 berekeningen
-# Naam student:
+# Naam student: Roisin
 # Groep:
 
 # Hier komt je code...
 
-c = ...
-f = ...
+#Hier komen de variabelen??
+celsius = "c"
+fahrenheit = "f"
 
-print()
+celsius = 62.2 
+
+#Berekening omrekenen van Celsius naar Fahrenheit
+fahrenheit = (celsius * 1.8) + 32
+
+print("%.2f Graden Celsius is gelijk aan %.2f Fahrenheit" % (celsius, fahrenheit))
+
+fahrenheit = 96
+
+#Berekening omrekenen van Fahrenheit naar Celsius
+celsius = (fahrenheit - 32) * 5/9
+
+print("%.2f Graden Fahrenheit is gelijk aan %.2f Celsius" % (fahrenheit, celsius))
