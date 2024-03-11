7093abe1975384a6a9702b94dcb22c085d8d2867
Rotisary1999 <97038433@st.deltion.nl>
2024-03-11 10:26:57 +0100

    Opdracht 2 Loops

--- opdrachten/060_loops/opdr_1/opdr_1.py
+++ opdrachten/060_loops/opdr_1/opdr_1.py
@@ -5,3 +5,6 @@
 # Hier komt je code, maak gebruik van de input functie om de lengte van de rechthoekzijden van de driehoek op te vragen.
 
 # Hier start de for-loop....
+cijfers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+for cijfer in cijfers:
+    print(cijfer)
--- opdrachten/060_loops/opdr_2/opdr_2.py
+++ opdrachten/060_loops/opdr_2/opdr_2.py
@@ -1,10 +1,11 @@
 # Opdracht 2 loops
-# Naam student:
+# Naam student: Roisin
 # Groep:
 
 # Hier komt je code...
 
 # Hier start de for-loop
 
-my_list = []
+for x in range(10, 51, 10):
+    print(x)
 
