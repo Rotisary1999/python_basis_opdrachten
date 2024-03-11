d61a283b6acdb23dec6d17f47eef89198b95fa55
Rotisary1999 <97038433@st.deltion.nl>
2024-03-11 10:58:49 +0100

    Opdracht 3 input functie

--- opdrachten/060_loops/opdr_3/opdr_3.py
+++ opdrachten/060_loops/opdr_3/opdr_3.py
@@ -5,5 +5,15 @@
 # Hier komt je code...
 
 # Hier start de for-loop
+for x in range(3, 81, 3):
+    print(x)
 
-my_list = []
+
+#Doe alle getallen uit de range in het kwadraat en deel ze vervolgens door 3
+lst = []
+for v in range(3, 81, 3):
+    lst.append((v ** 2)/3) 
+
+print(lst)
+
+
