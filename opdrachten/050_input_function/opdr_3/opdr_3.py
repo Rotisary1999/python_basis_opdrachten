63affb40f26177013b5603971510b6db36286bd1
Rotisary1999 <97038433@st.deltion.nl>
2024-03-14 14:07:00 +0100

    Opdrachten

--- opdrachten/050_input_function/opdr_3/opdr_3.py
+++ opdrachten/050_input_function/opdr_3/opdr_3.py
@@ -1,6 +1,14 @@
 # Opdracht 3 input functie
-# Naam student:
+# Naam student: Roisin
 # Groep:
 
 # Hier komt je code...
 
+my_songs = []
+
+my_songs.insert(0, input("Song 1: "))
+my_songs.insert(1, input("Song 2: "))
+my_songs.insert(2, input("Song 3: "))
+my_songs.insert(3, input("Song 4: "))
+my_songs.insert(4, input("Song 5: "))
+print(sorted(my_songs, reverse=True))
